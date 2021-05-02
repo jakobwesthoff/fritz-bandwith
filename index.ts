5c34d5690e16bac627b14650604f3f97057d8557
Jakob Westhoff <jakob@westhoffswelt.de>
2021-05-02 18:32:59 +0200

    Configuration: Allow for interactive configuration
    Configuration is now asked for via commandline and stored for later
    usage of the tool. Therefore initially the user is once asked to provide
    login credentials. Afterwards the stored credentials are used instead.

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
+#!/usr/bin/env node
+
 import { Fritzbox } from "@seydx/fritzbox";
 import logUpdate from "log-update";
 import chalk from "chalk";
 import prettyBytes from "pretty-bytes";
+import Conf from "conf";
+import inquirer from "inquirer";
 
-const USERNAME="";
-const PASSWORD="";
+interface Configuration {
+  username: string;
+  password: string;
+}
 
 interface Bandwidth {
   time: Date;
@@ -14,6 +20,7 @@ interface Bandwidth {
 }
 
 let box: Fritzbox;
+let config: Configuration;
 
 // const sum = (...value: string[]): number =>
 //   value.reduce((a, v) => a + parseInt(v), 0);
@@ -41,17 +48,19 @@ function render(bandwidth: Bandwidth): string {
   let output: string[] = [];
 
   output.push(
-    `${chalk.green( "⬆︎")}  ${prettyBytes(bandwidth.avgBpsUpstreamPerSecond, { bits: true })}/sec.`
+    `${chalk.green("⬆︎")}  ${prettyBytes(bandwidth.avgBpsUpstreamPerSecond, {
+      bits: true,
+    })}/sec.`
   );
   output.push(
-    `${chalk.yellow(
-      "⬇︎"
-    )}  ${prettyBytes(bandwidth.avgBpsDownstreamPerSecond, { bits: true})}/sec.`
+    `${chalk.red("⬇︎")}  ${prettyBytes(bandwidth.avgBpsDownstreamPerSecond, {
+      bits: true,
+    })}/sec.`
   );
   output.push(
-    `${chalk.cyan(
-      "⥥"
-    )}  ${prettyBytes(bandwidth.avgBpsMulticastPerSecond, { bits: true})}/sec.`
+    `${chalk.cyan("⥥")}  ${prettyBytes(bandwidth.avgBpsMulticastPerSecond, {
+      bits: true,
+    })}/sec.`
   );
 
   return output.join("\n");
@@ -63,13 +72,52 @@ async function loop() {
   setTimeout(loop, 5000);
 }
 
-(async () => {
-  try {
-    box = new Fritzbox({
-      username: USERNAME,
-      password: PASSWORD,
-    });
+function getConfig(): Configuration {
+  const config = new Conf<Configuration>();
+  return {
+    username: config.get("username", ""),
+    password: config.get("password", ""),
+  };
+}
 
+async function askForConfiguration(): Promise<void> {
+  const newConfig = await inquirer.prompt<Configuration>([
+    { type: "value", message: "Username: ", name: "username" },
+    { type: "password", message: "Password: ", name: "password" },
+  ]);
+  console.log("");
+
+  const conf = new Conf<Configuration>();
+  conf.set(newConfig);
+  config = newConfig;
+}
+
+async function login(): Promise<void> {
+  box = new Fritzbox({
+    username: config.username,
+    password: config.password,
+  });
+
+  try {
+    const info = await box.exec(
+      "urn:dslforum-org:service:DeviceInfo:1",
+      "GetInfo"
+    );
+    const modelName = info["NewModelName"];
+    console.log(`${chalk.yellow('■')}  Bandwidth (every 5s) of ${modelName}`);
+    console.log("");
+  } catch (e) {
+    console.log(chalk.red("Error accessing fritz box:"), e.message);
+    await askForConfiguration();
+    return login();
+  }
+}
+
+(async () => {
+  config = getConfig();
+  await login();
+
+  try {
     await loop();
   } catch (e) {
     console.error(`FATAL ERROR: `, e);
